956cfd3b829c6ddb339f0260526678c89e8b8310
Aradhya kumar mishra <116092894+Aradhyadeveloper@users.noreply.github.com>
2023-02-05 17:37:17 +0530

    Add files via upload

---
+++ productData.js
@@ -0,0 +1,37 @@
+export default[
+    {
+        id:1,
+        title:"Samsung Galaxy S7",
+        price:700.0,
+        img:"https://images.pexels.com/photos/1042143/pexels-photo-1042143.jpeg?auto=compress&cs=tinysrgb&w=600",
+        quantity:1,
+    },
+    {
+        id: 2,
+        title: "Moto G5 Plus",
+        price: 600.0,
+        img: "https://cdn.pixabay.com/photo/2016/11/20/08/33/camera-1842202__340.jpg",
+        quantity: 1,
+    },
+    {
+        id: 3,
+        title: "Xiomi Redmi Note 2 ",
+        price: 500.0,
+        img: "https://cdn.pixabay.com/photo/2017/04/03/15/52/mobile-phone-2198770__340.png",
+        quantity: 1,
+    },
+    {
+        id: 4,
+        title: "Nokia G20",
+        price: 400.0,
+        img: "https://images.pexels.com/photos/63690/pexels-photo-63690.jpeg?auto=compress&cs=tinysrgb&w=600",
+        quantity: 1,
+    },
+    {
+        id: 5,
+        title: "Apple i Phone",
+        price: 4000.0,
+        img: "https://images.pexels.com/photos/163065/mobile-phone-android-apps-phone-163065.jpeg?auto=compress&cs=tinysrgb&w=600",
+        quantity: 1,
+    },
+]
